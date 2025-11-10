b41b5122220d761fc45a448024ef9910c7cf5b6b
zoeaika <saudizoe@gmail.com>
2025-11-10 14:58:47 +0300

    Add next config

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
+
+module.exports = nextConfig
